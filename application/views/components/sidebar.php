3880ef37441508ab5e5da141a0e24394185a01b7
putra <putrayuszar96@gmail.com>
2021-09-03 23:37:12 +0700

    adjust sidebar

--- application/views/components/sidebar.php
+++ application/views/components/sidebar.php
@@ -6,31 +6,24 @@
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
                 </a>
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#menu-kantor" aria-expanded="false" aria-controls="collapseLayouts">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>Kantor Cabang <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="menu-kantor" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="#">Pegawai</a>
+                        <a class="nav-link" href="#">Divisi</a>
+                    </nav>
+                </div>
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#menu-gudang" aria-expanded="false" aria-controls="collapseLayouts">
                     <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>Gudang <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
                 <div class="collapse" id="menu-gudang" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="#">Gudang Langsa</a>
+                        <a class="nav-link" href="#">Rak</a>
+                        <a class="nav-link" href="#">Barang</a>
                     </nav>
                 </div>
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#menu-rak" aria-expanded="false" aria-controls="collapseLayouts">
-                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>Rak <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="menu-rak" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="#">Rak Cabang</a>
-                        <a class="nav-link" href="#">Pengaturan Rak</a>
-                    </nav>
-                </div>
-                <a class="nav-link" href="#">
-                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                    Barang
-                </a>    
-                <a class="nav-link" href="#">
-                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                    Pegawai
-                </a>    
             </div>
         </div>
         <div class="sb-sidenav-footer">
